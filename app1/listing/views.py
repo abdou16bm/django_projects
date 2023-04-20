fc1664859d7318b6703a29f4cb97e0fbddc32f9d
Abderrahmane Bouchama <abdou16bm@yahoo.fr>
2023-04-21 00:58:56 +0100

    use model in view

--- app1/listing/views.py
+++ app1/listing/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listing.models import Product
 
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("<h1>test view content  </h1>")
+    product = Product.objects.all()
+    print(product[0].name)
+#     return HttpResponse("<h1>test view content  </h1>")
+    return HttpResponse(f"""
+            <h1>Hello Django !</h1>
+            <p>Mes groupes préférés sont :<p>
+            <ul>
+                <li>{product[0].name}</li>
+                <li>{product[1].name}</li>
+                <li>{product[2].name}</li>
+            </ul>
+    """)
+
