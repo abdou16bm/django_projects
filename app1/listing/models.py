a6d2b9f364ce7821e452d1d5340b34cb7a97bd25
Abderrahmane Bouchama <abdou16bm@yahoo.fr>
2023-04-23 19:56:43 +0100

    update model Product

--- app1/listing/models.py
+++ app1/listing/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Product (models.Model):
     name = models.fields.CharField(max_length=100)
+    category = models.fields.CharField(max_length=25)
+    date_in = models.fields.IntegerField(default=2023, validators=[MinValueValidator(1900), MaxValueValidator(2030)])
+    status = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
